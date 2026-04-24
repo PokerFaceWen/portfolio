b1d61489873d4b6cffe431b00429e0b60932b510
PokerFaceWen <17889786156@163.com>
2026-04-24 21:14:50 +0800

    feat: initialize Astro project with GitHub Pages deployment

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://pokerfacewen.github.io',
+  base: '/portfolio',
+  output: 'static',
+});
